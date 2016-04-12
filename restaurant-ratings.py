8c831c3e793f537c7878a677ab2ee912b641776d
Hackbright Student <info@hackbrightacademy.com>
2016-04-11 18:05:25 -0700

    after code review

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,31 +1,30 @@
 # your code goes here
 
 def alph_restaurant_ratings(filename):
+    """print
+    """
 
     restaurants = {}
-    scores = open(filename)
+    scores_file = open(filename)
 
     #create dictionary from lines in text file that we opened above
-    for line in scores:
+    for line in scores_file:
         line = line.rstrip()
-        words = line.split(":")
+        names_and_scores = line.split(":")
         
         #setting the keys and values 
-        restaurants[words[0]] = words[1]
+        restaurants[names_and_scores[0]] = names_and_scores[1]
 
+    scores_file.close()
     #creates a new list of sorted key names
-    sorted_restaurants = sorted(restaurants)
+    sorted_restaurants = sorted(restaurants.keys())
 
-    counter = 0
 
     # using our sorted list of keys to search the 
     # dictionary "restaurants" for corresponding values 
     for restaurant in sorted_restaurants:
-        key = sorted_restaurants[counter]
-        value = restaurants[sorted_restaurants[counter]]
+        rating = restaurants[restaurant]
 
-        counter += 1
-
-        print key + " is rated at " + value
+        print restaurant + " is rated at " + rating
 
 alph_restaurant_ratings("scores.txt")
